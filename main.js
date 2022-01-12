694d6f9f32bd2a7b2688b58694f797e4839e5e99
Noah Taggart <noahtag@gmail.com>
2022-01-12 11:39:25 -0600

    deconstructed instructions on playing trombone

---
+++ main.js
@@ -0,0 +1,86 @@
+/* playing trombone 
+1. Set trombone case on ground
+2. Make sure brand logo is facing up
+3. unlatch case
+4. open case
+5. unhook trombone slide
+6. remove trombone slide from case
+7. put stopper of trombone slide on ground,
+    holding the handle so the slide is verticle
+8. remove bell from case
+9. line up bell and slide attachement
+10. Insert bell into slide
+11. Twist the screw at the attachment to lock bell and slide in place
+12. remove mouthpiece from case
+13. insert mouthpiece into leadpipe
+14. turn the mouthpiece a quarter way around
+15. lift trombone to face
+16. buzz lips into mouthpiece
+*/
+
+console.log('Begin playing Trombone')
+const groundCase = () => {
+    console.log('1. Set trombone case on ground')
+}
+const logoUp = () => {
+    console.log('2. Make sure brand logo is facing up')
+}
+const unlatch = () => {
+    console.log('3. unlatch case')
+}
+const open = () => {
+    console.log('4. open case')
+}
+const unhook = () => {
+    console.log('5. unhook trombone slide')
+}
+const removeSlide = () => {
+    console.log('6. remove trombone slide from case')
+}
+const stopper = () => {
+    console.log('7. put stopper of trombone slide on ground, holding the handle so the slide is verticle')
+}
+const removeBell = () => {
+    console.log('8. remove bell from case')
+}
+const lineUp = () => {
+    console.log('9. line up bell and slide attachement')
+}
+const insertBell = () => {
+    console.log('10. Insert bell into slide')
+}
+const screw = () => {
+    console.log('11. Twist the screw at the attachment to lock bell and slide in place')
+}
+const removeMouthpiece = () => {
+    console.log('12. remove mouthpiece from case')
+}
+const insertMouthpiece = () => {
+    console.log('13. insert mouthpiece into leadpipe')
+}
+const quarterTurn = () => {
+    console.log('14. turn the mouthpiece a quarter way around')
+}
+const lift = () => {
+    console.log('15. lift trombone to face')
+}
+const buzz = () => {
+    console.log('16. buzz lips into mouthpiece')
+}
+
+groundCase()
+logoUp()
+unlatch()
+open()
+unhook()
+removeSlide()
+stopper()
+removeBell()
+lineUp()
+insertBell()
+screw()
+removeMouthpiece()
+insertMouthpiece()
+quarterTurn()
+lift()
+buzz()
